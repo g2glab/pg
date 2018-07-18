d19dede157cb083b05da6e947ade9df6f2c693d9
matsusho <matsusho070@gmail.com>
2018-07-19 00:31:51 +0900

    use vis_label for graphviz label if possible

--- src/pg_to_dot.js
+++ src/pg_to_dot.js
@@ -38,11 +38,17 @@ rl.on('line', function(line) {
     pg.checkItems(items);
     items = items.map(item => item.replace(/"/g,'')); // remove double quotes
     types = types.map(type => type.replace(/"/g,'')); // remove double quotes
+    
     if (pg.isNodeLine(line)) {
       var id = items[0];
-      var output = [];
-      content += '"' + id + '" [label="' + id + ': ' + types.join(';') + '\\l';
+      var topLabel = id;
+      var visIndex = items.indexOf('vis_label');
+      if(visIndex >= 0) {
+        topLabel = items[visIndex + 1];
+      }
+      content += '"' + id + '" [label="' + types.join(';') + '\\l' + topLabel + '\\l';
       for (var i=1; i<items.length-1; i=i+2) {
+        if(i == visIndex) continue;
         content += items[i] + ': ' + items[i+1] + '\\l'; 
       }
       content += '"]\n'
