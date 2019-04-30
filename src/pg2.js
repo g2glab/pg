d3a4505fa57593815f936f71aa08a21df2cb293e
ryotayamanaka <ryotayamanaka@gmail.com>
2019-04-30 05:33:56 +0000

    use Set object for labels

--- src/pg2.js
+++ src/pg2.js
@@ -1,6 +1,6 @@
-var commander = require('commander');
-var path = require('path');
-var fs = require('fs');
+let commander = require('commander');
+let path = require('path');
+let fs = require('fs');
 
 exports.commander = commander
   .version(require("../package.json").version)
@@ -25,9 +25,9 @@ exports.isLineRead = function (line) {
 }
 
 exports.extractItems = function (line) {
-  var regexNode = /^("[^"]+"|[^"\s]+)/;
-  var regexEdge = /^("[^"]+"|[^"\s]+)\s+(->|--)\s+("[^"]+"|[^"\s]+)/;
-  var id1, id2, undirected;
+  let regexNode = /^("[^"]+"|[^"\s]+)/;
+  let regexEdge = /^("[^"]+"|[^"\s]+)\s+(->|--)\s+("[^"]+"|[^"\s]+)/;
+  let id1, id2, undirected;
   if (!(result = regexEdge.exec(line))) {
     if (!(result = regexNode.exec(line))) {
       console.log('ERROR - this line is neither node nor edge: ' + line);
@@ -41,17 +41,25 @@ exports.extractItems = function (line) {
     id2 = [result[3].rmdq(), result[3].type()];
     undirected = (result[2] == '->') ? false : true ;
   }
-  var labels = globalGroupMatch(line, /\s:("[^"]+"|[^"\s]+)/g).map((m) => m[1].rmdq());
+  // LABELS
+  let labels = new Set();
+  let regexLabels = /\s:("[^"]+"|[^"\s]+)/g;
+  //let labels = globalGroupMatch(line, regexLabels).map((m) => m[1].rmdq());
+  while (result = regexLabels.exec(line)) {
+    labels.add(result[1].rmdq());
+  }
+  // PROPERTIES
   let properties = new Map();
-  globalGroupMatch(line, /\s("[^"]+"|[^"\s]+):("[^"]*"|[^"\s]*)/g).forEach(function(m) {
-    properties.set(m[1].rmdq(), m[2]);
-  });
-  return [id1, id2, undirected, labels, properties];
+  let regexProperties = /\s("[^"]+"|[^"\s]+):("[^"]*"|[^"\s]*)/g;
+  while (result = regexProperties.exec(line)) {
+    properties.set(result[1].rmdq(), result[2]);
+  }
+  console.log(Array.from(labels));
+  return [id1, id2, undirected, Array.from(labels), properties];
 }
 
 exports.checkItems = function (items) {
-  console.log('PG: ' + items);
-  for(var i=0; i<items.length; i++){
+  for(let i = 0; i < items.length; i++){
     if (items[i].match(/\t/)) {
       console.log('WARNING: This item has unexpected tab(\\t): [' + items[i] + '] after [' + items[i-1] + ']');
     }
@@ -86,47 +94,3 @@ function isDoubleQuoted(str) {
   }
 }
 
-function globalGroupMatch(text, pattern) {
-  var matchedArray = [];
-  var regex = pattern;
-  while(match = regex.exec(text)) {
-    matchedArray.push(match);
-  }
-  return matchedArray;
-}
-
-// NOT USED. TO BE REMOVED.
-exports.extractTypes = function (line) {
-  var types = globalGroupMatch(line, /\s:(\w+|"[^"]+")/g).map((m) => m[1]);
-  line = line.replace(/\s:(\w+|"[^"]+")/g, ''); // remove types
-  return [line, types];
-}
-
-// NOT USED. TO BE REMOVED.
-function splitBySpace(str) {
-  var inQuote = false;
-  var afterQuote = false;
-  var tokens = [];
-  var token = '';
-  for (var i = 0; i < str.length; i++) {
-    var c = str[i];
-    if(afterQuote && c === '"') {
-      tokens[tokens.length - 1] += '"';
-      afterQuote = false;
-      continue;
-    }
-    afterQuote = false;
-    if(c === '"'){
-      inQuote = !inQuote;
-      afterQuote = true;
-    }
-    if(!inQuote && c.match(/\s/)){
-      if(token != '') tokens.push(token);
-      token = '';
-    }else{
-      token += c;
-    }
-  }
-  if(token != '') tokens.push(token);
-  return tokens;
-}
