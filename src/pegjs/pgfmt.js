00c5c5cc3520c5518de2f8eafd914997e6548cc5
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-19 04:40:32 +0900

    modify syntax error reporting message

--- src/pegjs/pgfmt.js
+++ src/pegjs/pgfmt.js
@@ -43,14 +43,21 @@ try {
   const startCol = err.location.start.column;
   const endCol = err.location.end.column;
   if (startLine == endLine) {
-    console.error(`ERROR line:${startLine}(col:${startCol})`);
+    console.error(`ERROR line:${startLine}(col:${startCol}-${endCol})`);
   } else {
-    console.error(`ERROR: line ${startLine}(col:${startCol})-${endLine}(col:${endCol})`);
+    console.error(`ERROR line:${startLine}(col:${startCol})-${endLine}(col:${endCol})`);
   }
   console.error(err.message);
-  console.error('--')
-  inputText.split('\n').slice(startLine-1, endLine).forEach((line) => {
-    console.error(setBackgroundRed(line.substring(0, startCol - 1)) + line.substring(startCol - 1));
+  console.error('--');
+  const lines = inputText.split('\n').slice(startLine-1, endLine);
+  lines.forEach((line, i) => {
+    if (i == 0) {
+      console.error(makeRed(line.substring(0, startCol - 1)) + line.substring(startCol - 1));
+    } else if (i < lines.length - 1) {
+      console.error(makeRed(line));
+    } else {
+      console.error(makeRed(line.substring(0, endCol)) + line.substring(endCol));
+    }
   });
   process.exit(1);
 }
@@ -222,14 +229,9 @@ function checkGraph(objectTree) {
   });
 }
 
-function setForgroundRed(text) {
-  var redForground = '\u001b[31m';
-  var reset = '\u001b[0m';
-  return redForground + text + reset;
-}
-
-function setBackgroundRed(text) {
-  var redBackground = '\u001b[41m';
-  var reset = '\u001b[0m';
-  return redBackground + text + reset;
+function makeRed(text) {
+  // const red = '\u001b[31m'; // foreground
+  const red = '\u001b[41m'; // backgrond
+  const reset = '\u001b[0m';
+  return red + text + reset;
 }
