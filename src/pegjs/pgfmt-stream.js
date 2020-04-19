730358a4528753b74aa26886e54b4c565e23595d
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-20 05:33:33 +0900

    use stream for peg

--- src/pegjs/pgfmt-stream.js
+++ src/pegjs/pgfmt-stream.js
@@ -2,7 +2,9 @@
 
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const parser = require('./pg_parser.js');
+
 
 const commander = require('commander')
       .option('-f, --format <FORMAT>', 'json, neo')
@@ -18,10 +20,57 @@ const commander = require('commander')
 if(commander.args[0]) {
   const inputFile = commander.args[0];
   const src = fs.createReadStream(inputFile, 'utf8');
-  src.pipe(process.stdout);
+  const reader = readline.createInterface({input:src});
+  reader.on('line', (line) => {
+    outputNeo(line);
+  });
+  reader.on('close', () => {
+    // console.error('End!');
+  });
 } else if (process.stdin.isTTY) {
   commander.help();
 } else {
-  // inputText = fs.readFileSync(process.stdin.fd).toString();
   // outFilePrefix = 'pgfmt';
+  const reader = readline.createInterface(process.stdin);
+  reader.on('line', (line) => {
+    outputNeo(line);
+  });
+  process.stdin.on('end', () => {
+    // console.error('End!');
+  });
+}
+
+// Functions
+function outputNeo(line) {
+  if (line.charAt(0) != '#' && line != '') {
+    const objectTree = parser.parse(line);
+    const nodeProps = Object.keys(objectTree.nodeProperties);
+    const edgeProps = Object.keys(objectTree.edgeProperties);
+    objectTree.nodes.forEach(n => {
+      let line = [];
+      line.push(n.id)
+      line.push(n.labels)
+      nodeProps.forEach(p => {
+        if (n.properties[p]) {
+          line.push(n.properties[p].join(';'));
+        } else {
+          line.push('');
+        }
+      });
+      console.log(line.join('\t'));
+    });
+    objectTree.edges.forEach(e => {
+      let line = [];
+      line.push(e.from, e.to)
+      line.push(e.labels)
+      edgeProps.forEach(p => {
+        if (e.properties[p]) {
+          line.push(e.properties[p].join(';'));
+        } else {
+          line.push('');
+        }
+      });
+      console.log(line.join('\t'));
+    });
+  }
 }
