9cada526f0bcba39c42f2d38b35e69b3b1bc296f
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2020-05-16 16:27:29 +0900

    Change default number of worker processes to 1

--- src/pg2.js
+++ src/pg2.js
@@ -5,7 +5,7 @@ let fs = require('fs');
 exports.commander = commander
   .version(require("../package.json").version)
   .option('-o, --output_dir [dir]', 'directory path for results', './')
-  .option('-p, --parallel [num]', 'number of worker processors to use', '-1')
+  .option('-p, --parallel [num]', 'number of worker processors to use', '1')
   .option('--without_tmp_file', 'prohibit creation of temporary files (slow, but works in smaller disk space)')
   .arguments('<pg_file_path>')
   .action(function (pg_file_path) {
